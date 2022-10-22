b6fa1899c679a14b8f8a212100bc39c8bdf0454b
remaleino <raimond.leino@gmail.com>
2022-10-22 14:27:28 +0300

    Google login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,39 @@
-function LoginPage() {
-    return <h1>Welcome to the LogIn!</h1>
+import React from 'react'
+import { useSession, signIn, signOut, getSession, GetSessionParams } from 'next-auth/react'
+
+const Login = () => {
+    const { data: session } = useSession()
+    if (session) {
+        return (
+            <div>
+                <p>Welcome, {session?.user?.email}</p>
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        )
+    } else {
+        //TÄÄHN DESIGN
+        return (
+            <div>
+                <p>You are not signed in.</p>
+                <button onClick={() => signIn()}>Sign in</button>
+            </div>
+        )
+    }
 }
 
-export default LoginPage
+export default Login
+
+export const getServerSideProps = async (context: GetSessionParams | undefined) => {
+    const session = await getSession(context)
+
+    if (session) {
+        return {
+            redirect: {
+                destination: '/'
+            }
+        }
+    }
+    return {
+        props: { session }
+    }
+}
