6e7315f37b4a2b391def0fda9b18677ae6e00626
jevgenix <jekka9803@gmail.com>
2022-10-22 12:29:44 +0300

    pages created

---
+++ pages/login.tsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+    return <h1>Welcome to the LogIn!</h1>
+}
+
+export default LoginPage
---
+++ pages/register.tsx
@@ -0,0 +1,5 @@
+function Register() {
+    return <h1>Welcome to the Register!</h1>
+}
+
+export default Register
