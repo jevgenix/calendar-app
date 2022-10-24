5f5e791de0bae12b7c04f2d51911cae4d232e795
jevgenix <jekka9803@gmail.com>
2022-10-24 21:36:24 +0300

    example added

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,47 +1,25 @@
 import React from 'react'
-import { useSession, signIn, signOut, getSession, GetSessionParams } from 'next-auth/react'
-import Head from "next/head"
+import { useSession, signOut, getSession, GetSessionParams } from 'next-auth/react'
+import LoginContainer from '../components/LoginPage/LoginContainer'
+
 
 const Login = () => {
     const { data: session } = useSession()
-    const LoginForm = () => {
-        return (
-            <form action="/send-data-here" method="post">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" required />
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" required />
-                <button type="submit">Log in</button>
-                <button type="submit">Register</button>
-            </form>
-        )
-    }
     if (session) {
         return (
-            <div>
-
+            < div >
                 <main>
                     {/* main page content  */}
-
                     <p>Welcome, {session?.user?.email}</p>
                     <button onClick={() => signOut()}>Sign out</button>
                 </main>
-            </div>
+            </div >
         )
     } else {
-        //TÄÄHN DESIGN
+        // https://www.pinterest.com/#search
         return (
-            <div>
-                <Head>
-                    <title>Login </title>
-                </Head>
-                <h1> Hello </h1>
-                <p>You are not signed in.</p>
-                <LoginForm />
 
-                <button onClick={() => signIn()}>Sign in with Google</button>
-                {/* <button onClick={() => console.log("log in!")}>Log in</button> */}
-            </div>
+            <LoginContainer />
         )
     }
 }
