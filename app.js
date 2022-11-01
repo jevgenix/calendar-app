5d07df9be33d3f325c9b7e5e1f3d9365145fff3d
jevgenix <jekka9803@gmail.com>
2022-11-01 09:31:07 +0200

    deploy to heroku

--- app.js
+++ app.js
@@ -24,13 +24,17 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 
+
+
 (async () => {
     try {
         await app.prepare();
         const server = express();
+
         server.all("*", (req, res) => {
             return handle(req, res);
         });
+
         server.listen(port, (err) => {
             if (err) throw err;
             console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
@@ -39,6 +43,8 @@ const port = process.env.PORT || 3000;
         console.error(e);
         process.exit(1);
     }
+
+
 })();
 
 
